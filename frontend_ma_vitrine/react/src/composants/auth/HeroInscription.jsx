18bff3095fbbf3cf5ad1a04e887cde0541d0c127
STEPHANE FODJO <142510640+FODJO1274@users.noreply.github.com>
2026-07-15 13:55:46 +0100

    feat:Hero de l'inscription

---
+++ frontend_ma_vitrine/react/src/composants/auth/HeroInscription.jsx
@@ -0,0 +1,17 @@
+function HeroInscription() {
+
+    return (
+        <div className="col-lg-6 d-none d-lg-flex bg-success text-white justify-content-center align-items-center">
+            <div className="text-center px-5">
+                <h1 className="display-5 fw-bold">
+                    Rejoignez Ma Vitrine Locale
+                </h1>
+                <p className="lead mt-4">
+                    Créez votre compte pour acheter ou vendre des produits artisanaux.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default HeroInscription;
