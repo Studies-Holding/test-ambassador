2290b07d013451645fdcb3bbad0fa5aaff14ca58
STEPHANE FODJO <142510640+FODJO1274@users.noreply.github.com>
2026-07-15 13:52:17 +0100

    feat: mise à jour de l'url et ajout de la fonction de recupération d'un user

--- frontend_ma_vitrine/react/src/services/serviceAuthenti.js
+++ frontend_ma_vitrine/react/src/services/serviceAuthenti.js
@@ -1,14 +1,63 @@
 import API_URL from "./api";
 
+export async function signUp(user) {
+    const response = await fetch(`${API_URL}/auth/signup`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"  
+        },
+        body: JSON.stringify(user)
+    });
+
+    const data = await response.json();
+    if(!response.ok) {
+        throw new Error(data.message);
+    }
+    return data;
+}
+
 export async function login(user) {
 
-    const response = await fetch(`${API_URL}/users?email=${user.email}`);
+    const response = await fetch(`${API_URL}/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    });
 
+    const data = await response.json();
     if(!response.ok) {
-        throw new Error("Impossible de contacter le serveur.");
+        throw new Error(data.message);
     }
-    const users = await response.json();
-    const foundUser = users.find((currentUser) => 
-        currentUser.password === user.password);
-    return foundUser || null;
+    
+    return data;
+}
+
+export async function getMe() {
+
+    const token = localStorage.getItem("token");
+
+    const response = await fetch(`${API_URL}/auth/me`, {
+
+        method: "GET",
+
+        headers: {
+
+            Authorization: `Bearer ${token}`
+
+        }
+
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+
+        throw new Error(data.message);
+
+    }
+
+    return data;
+
 }
